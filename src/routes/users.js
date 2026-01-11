0f72ea828a4135e770ad6fd46e2d4f76acbbcc68
Juan Daniel <juankycabj05@gmail.com>
2026-01-11 20:24:46 -0300

    Importacion correcta en routes/users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { login } from '../controllers/usersControllers.js';
+import { login } from '../controllers/usersController.js';
 
 const router = Router();
 
