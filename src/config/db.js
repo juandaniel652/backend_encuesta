ba972000b53492d5b52ef57aaf22de2f5f4ee9b3
Juan Daniel <juankycabj05@gmail.com>
2026-01-11 21:19:10 -0300

    db.js con variables de entronor

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,12 @@
-const { createClient } = require('@supabase/supabase-js');
-require('dotenv').config();
+import { createClient } from '@supabase/supabase-js';
+
+if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
+  throw new Error('Missing Supabase environment variables');
+}
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.SUPABASE_ANON_KEY
 );
 
-module.exports = supabase;
+export default supabase;
