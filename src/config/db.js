9ac95c027f15c502bb83af58f51bd2af43139189
Juan Daniel <juankycabj05@gmail.com>
2026-01-30 15:07:42 -0300

    db.js actualizado

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,10 @@
-import { createClient } from '@supabase/supabase-js';
-import 'dotenv/config';
+import mysql from 'mysql2/promise';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
 
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Supabase env vars missing');
-}
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export default db;
