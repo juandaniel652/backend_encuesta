be85661e2f4b174a5c2b17d1c7c5a6ed04d75a03
Juan Daniel <juankycabj05@gmail.com>
2026-02-01 15:22:25 -0300

    en controllers/campaignsController.js importacion de Supabase para su uso y uso adecuado de try y catch

--- src/controllers/campaignsController.js
+++ src/controllers/campaignsController.js
@@ -1,4 +1,5 @@
 import * as CampaignService from '../services/campaignsService.js';
+import { supabase } from '../config/supabaseClient.js';
 
 
 export async function getCampaigns(req, res, next) {
@@ -12,29 +13,35 @@ export async function getCampaigns(req, res, next) {
 
 
 export const createCampaign = async (req, res) => {
+  try {
+    console.log('🔥 BODY RECIBIDO:', req.body);
 
-  console.log('🔥 BODY RECIBIDO:', req.body);
+    const { name, client_type, date_start, date_end } = req.body;
 
-  const { name, client_type, date_start, date_end } = req.body;
+    if (!name || !client_type) {
+      return res.status(400).json({ error: "Invalid campaign data" });
+    }
 
-  if (!name || !client_type) {
-    return res.status(400).json({ error: "Invalid campaign data" });
+    const { data, error } = await supabase
+      .from('campaigns')
+      .insert([{
+        name,
+        client_type,
+        date_start,
+        date_end
+      }])
+      .select()
+      .single();
+
+    if (error) {
+      console.error('❌ Supabase error:', error);
+      return res.status(500).json({ error: error.message });
+    }
+
+    res.json(data);
+
+  } catch (err) {
+    console.error('💥 Server crash:', err);
+    res.status(500).json({ error: err.message });
   }
-
-  const { data, error } = await supabase
-    .from('campaigns')
-    .insert([{
-      name,
-      client_type,
-      date_start,
-      date_end
-    }])
-    .select()
-    .single();
-
-  if (error) {
-    return res.status(500).json({ error: error.message });
-  }
-
-  res.json(data);
 };
